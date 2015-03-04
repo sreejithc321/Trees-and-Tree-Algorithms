d9a89b392db9eb8389bfbc078893aeeff8aeb13f
sreejith <sreejithc321@gmail.com>
2015-03-04 09:50:07 +0530

    binary tree using class

---
+++ bin_tree_class.py
@@ -0,0 +1,53 @@
+
+class BinaryTree():
+	
+	def __init__(self,root):
+		self.key = root
+		self.right_child = None
+		self.left_child = None
+		
+	def set_root_val(self,value):
+		self.key = value
+		
+	def insert_right_node(self,value):
+		if self.right_child == None :
+			self.right_child = BinaryTree(value)
+		else:
+			new_node = BinaryTree(value)
+			new_node.right_child = self.right_child
+			self.right_child = new_node
+			
+	def insert_left_node(self,value):
+		if self.left_child == None :
+			self.left_child = BinaryTree(value)
+		else:
+			new_node = BinaryTree(value)
+			new_node.left_child = self.left_child
+			self.left_child = new_node
+		
+	def get_left_child(self):
+		return self.left_child
+		
+	def get_right_child(self):
+		return self.right_child
+		
+	def get_root_val(self):
+		return self.key				
+
+
+# Testing
+
+r = BinaryTree('a')
+print r.get_root_val()
+print(r.get_left_child())
+r.insert_left_node('b')
+print(r.get_left_child())
+print(r.get_left_child().get_root_val())
+
+r.insert_right_node('c')
+print(r.get_right_child())
+print(r.get_right_child().get_root_val())
+
+r.get_right_child().set_root_val('test')
+print(r.get_right_child().get_root_val())
+
