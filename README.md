1a56efed1771c800895b90b938570d1017f839cf
sreejith <sreejithc321@gmail.com>
2015-03-06 21:34:45 +0530

    binary heap

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ If each node in the tree has a maximum of two children, we say that the tree is 
 - bin_tree_class.py
 
 ## Tree Traversals
-	- traversal.py
+- traversal.py
 ### Pre-order
     - Display the data part of root element (or current element)
     - Traverse the left subtree by recursively calling the pre-order function.
@@ -29,3 +29,19 @@ If each node in the tree has a maximum of two children, we say that the tree is 
     - Traverse the left subtree by recursively calling the post-order function.
     - Traverse the right subtree by recursively calling the post-order function.
     - Display the data part of root element (or current element).
+
+## Binary Heap
+
+In a heap, for every node x with parent p, the key in p is smaller than or equal to the key in x (heap order property)
+A binary heap perform both enqueue and dequeue items in O(logn)
+
+- BinaryHeap() creates a new, empty, binary heap.
+- insert(item) adds a new item to the heap.
+- find_min() returns the item with the minimum key value, leaving item in the heap.
+- del_min() returns the item with the minimum key value, removing the item from the heap.
+- is_empty() returns true if the heap is empty, false otherwise.
+- size() returns the number of items in the heap.
+- build_heap(list) builds a new heap from a list of keys.
+
+### Binary Heap Implementation (min heap)
+- bin_heap.py
---
+++ bin_heap.py
@@ -0,0 +1,76 @@
+# Min heap : smallest key is always at the front
+# Implementation on min heap using lists
+# Left child of a parent (at position p) is the node that is found in position 2p in the list
+# Right child of the parent is at position 2p+1 in the list.
+# For a node at position n in the list, the parent is at position n/2.
+# Heap order property : for every node x with parent p, the key in p is smaller than or equal to the key in x
+
+class BinHeap:
+
+    def __init__(self):
+        self.heap_list = [0]
+        self.current_size = 0
+
+
+    def pos_up(self,i):
+		# Apply heap oder property
+        while i // 2 > 0:
+          if self.heap_list[i] < self.heap_list[i // 2]:
+             tmp = self.heap_list[i // 2]
+             self.heap_list[i // 2] = self.heap_list[i]
+             self.heap_list[i] = tmp
+          i = i // 2
+
+    def insert(self,item):
+	  # Add new item to end of list 	
+      self.heap_list.append(item)
+      self.current_size = self.current_size + 1
+      # Find exact position
+      self.pos_up(self.current_size)
+
+    def pos_down(self,i):
+	''' Pushing the new root node down the tree to its proper position '''	
+	  # Repeat the swapping process with a node and its children until the node is 
+	  # swapped into a position on the tree where it is already less than both children
+      while (i * 2) <= self.current_size:
+          min_val = self.minChild(i)
+          if self.heap_list[i] > self.heap_list[min_val]:
+              tmp = self.heap_list[i]
+              self.heap_list[i] = self.heap_list[min_val]
+              self.heap_list[min_val] = tmp
+          i = min_val
+
+    def minChild(self,i):
+      if i * 2 + 1 > self.current_size:
+          return i * 2
+      else:
+          if self.heap_list[i*2] < self.heap_list[i*2+1]:
+              return i * 2
+          else:
+              return i * 2 + 1
+
+    def del_min(self):
+      retval = self.heap_list[1]
+      self.heap_list[1] = self.heap_list[self.current_size]
+      self.current_size = self.current_size - 1
+      self.heap_list.pop()
+      self.pos_down(1)
+      return retval
+
+    def build_heap(self,num_list):
+      i = len(num_list) // 2
+      self.current_size = len(num_list)
+      self.heap_list = [0] + num_list[:]
+      while (i > 0):
+          self.pos_down(i)
+          i = i - 1
+
+# Testing
+
+bh = BinHeap()
+bh.build_heap([9,5,6,2,3])
+print(bh.del_min())
+print(bh.del_min())
+print(bh.del_min())
+print(bh.del_min())
+print(bh.del_min())
